a3316da4767710a3863879beeaf1fada9fd69a19
rushyam <rushyamsonu@gmail.com>
2017-03-19 15:13:53 +0530

    Update Marker_task.cpp

--- Marker_task.cpp
+++ Marker_task.cpp
@@ -1,4 +1,20 @@
-#include "cv.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/flann/miniflann.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/photo/photo.hpp"
+#include "opencv2/video/video.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/ml/ml.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/contrib/contrib.hpp"
+#include "opencv2/core/core_c.h"
+#include "opencv2/highgui/highgui_c.h"
+#include "opencv2/imgproc/imgproc_c.h"
+#include "iostream"
+using namespace cv;
+using namespace std;
 int  dis(Point p1,Point p2);
 int main()
 {
